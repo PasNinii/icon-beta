98f650cb734237d0cd87bc09b57c1d4e703121f7
PasNinii <nicrannou@gmail.com>
2024-03-15 15:35:52 +0100

    f

--- load_image.sh
+++ load_image.sh
@@ -1,10 +1,12 @@
+# Download image based on args that should be valid icon name
 px=(20 24 40 48)
 icon=$1
 path=$(pwd)
+google_link="https://fonts.google.com/specimen/Material+Icons+Outlined?query=material+icons+outlined"
 
 for i in "${px[@]}"
 do
-  wget "https://fonts.gstatic.com/s/i/short-term/release/materialsymbolsoutlined/${icon}/wght300/${i}px.svg" -P "${path}/src/assets/icons/${icon}"
+  wget "${google_link}/${icon}/wght300/${i}px.svg" -P "${path}/src/assets/icons/${icon}"
 done
 
 
--- write_image.sh
+++ write_image.sh
@@ -1,10 +1,11 @@
 image_folder="./src/assets/icons/*"
 destination_path="./src/app/icons.ts"
 
+# create array of icon
 echo 'export const icons = [' > ${destination_path};\
   find "./src/assets/icons" -type f -name "*.svg"  | awk -F'/' '{print "\"" $5 "_" $6 "\"" "," }'\
   >> ${destination_path}; echo '];'\
   >> ${destination_path};
 
-
+# create icon type
 echo 'export type AppIcon = ' >> ${destination_path}; find ${image_folder} -type d | awk -F'/' '{print "\"" $5 "\"" "|" }' >> ${destination_path}; echo 'undefined;' >> ${destination_path};
